a1bfc9c013f299b07a526575b130e37150ae2715
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2024-01-06 16:08:17 +0530

    first commit

--- src/components/HOC/HOCimage.jsx
+++ src/components/HOC/HOCimage.jsx
@@ -3,7 +3,9 @@ import LikeCounter1 from "./HOC";
 
 function LikeImage({ likeCounter, handleLikeCount }) {
   return (
+    
     <div className="p-4">
+      
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={handleLikeCount}
